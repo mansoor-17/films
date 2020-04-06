6dba2a6ef8c51cdd0fa0cd68b31f4fd55c7345df
ip11 <iprash011@gmail.com>
2020-04-06 10:07:40 +0530

    Fixed Error Warning in Console

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,47 +1,51 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import '../App.css';
-function Movies({films}) {
+function Movies({ films }) {
 	return (
 		<Fragment>
 			<center>
 				<h1>Avengers Movies</h1>
 				<table>
-					<tr>
-						<th>Film Name</th>
-						<th>Release date</th>
-						<th>Director</th>
-					</tr>
-					<tr>
-						<td>Iron Man</td>
-						<td>May 2, 2008</td>
-						<td>Jon Favreau</td>
-					</tr>
-					<tr>
-						<td>The Incredible Hulk</td>
-						<td>June 13, 2008</td>
-						<td>Louis Leterrier</td>
-					</tr>
-					<tr>
-						<td>Thor</td>
-						<td> May 6, 2011</td>
-						<td>Kenneth Branagh</td>
-					</tr>
-					<tr>
-						<td>Captain America: The First Avenger</td>
-						<td>July 22, 2011</td>
-						<td>Joe Johnston</td>
-					</tr>
-					<tr>
-						<td>Guardians of the Galaxy</td>
-						<td>August 1, 2014</td>
-						<td>James Gunn</td>
-					</tr>
-					<tr>
-						<td>Ant-Man</td>
-						<td>July 17, 2015</td>
-						<td>Peyton Reed</td>
-					</tr>
+					<thead>
+						<tr>
+							<th>Film Name</th>
+							<th>Release date</th>
+							<th>Director</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Iron Man</td>
+							<td>May 2, 2008</td>
+							<td>Jon Favreau</td>
+						</tr>
+						<tr>
+							<td>The Incredible Hulk</td>
+							<td>June 13, 2008</td>
+							<td>Louis Leterrier</td>
+						</tr>
+						<tr>
+							<td>Thor</td>
+							<td> May 6, 2011</td>
+							<td>Kenneth Branagh</td>
+						</tr>
+						<tr>
+							<td>Captain America: The First Avenger</td>
+							<td>July 22, 2011</td>
+							<td>Joe Johnston</td>
+						</tr>
+						<tr>
+							<td>Guardians of the Galaxy</td>
+							<td>August 1, 2014</td>
+							<td>James Gunn</td>
+						</tr>
+						<tr>
+							<td>Ant-Man</td>
+							<td>July 17, 2015</td>
+							<td>Peyton Reed</td>
+						</tr>
+					</tbody>
 				</table>
 			</center>
 		</Fragment>
